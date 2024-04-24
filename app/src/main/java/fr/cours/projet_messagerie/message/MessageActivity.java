c63551f29cb6d75d75fbcaeec26724aec1c78fda
baptiste <baptiste.machu@orange.fr>
2024-04-24 13:28:57 +0200

    activité MessageActivity en construction

---
+++ app/src/main/java/fr/cours/projet_messagerie/message/MessageActivity.java
@@ -0,0 +1,71 @@
+package fr.cours.projet_messagerie.message;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.sql.Timestamp;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import fr.cours.projet_messagerie.R;
+
+public class MessageActivity extends AppCompatActivity {
+
+    private FirebaseFirestore mabd;
+
+    private RecyclerView messageRecycler;
+
+    private EditText messageTextSend;
+
+    private Button boutonSendMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.chat_message_recyclerview);
+        messageTextSend=findViewById(R.id.MessagetextZone);
+        boutonSendMessage=findViewById(R.id.boutonSendMessage); // Chargement du bouton de send message
+
+        messageRecycler = findViewById(R.id.recycler_messages); // Chargement du recycler view de messages
+
+
+    }
+
+    public void onClickSendMessage(View view) {
+        this.mabd = FirebaseFirestore.getInstance();
+
+        String messageText = messageTextSend.getText().toString();
+
+        if(!messageText.isEmpty()) {
+
+            // Get current timestamp
+            long timestamp = System.currentTimeMillis();
+
+            // Create a new message document in Firestore
+            Map<String, Object> message = new HashMap<>();
+            message.put("text", messageText);
+            message.put("timestamp", timestamp);
+            //message.put("uuidSender",uuidSender);
+            //message.put("uuidReceiver",uuidReceiver);
+
+
+
+        }
+
+    }
+}
